4f23a6b9aa0f19c955d87fb2dd5488f8dcaa3d44
Mario HD <mariohdpz@gmail.com>
2017-05-06 19:33:15 -0500

    Home page styling

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,28 @@ var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
 
-var paths = {
-    scss: './assets/sass/*.scss'
-};
 
 gulp.task('sass', function () {
-    return gulp.src(paths.scss)
+    return gulp.src(['style.scss'],{ base: '.' })
+        .pipe(sourcemaps.init())
         .pipe(sass({
             includePaths: require('node-bourbon').includePaths
         }))
-        .pipe(gulp.dest('.'));
+        .pipe(cssmin())
+        .pipe(sourcemaps.write('../maps'))
+        .pipe(gulp.dest('.'))
+        .pipe(browserSync.stream());
 });
+
+gulp.task('watch',function() {
+    browserSync.init({
+        proxy: "http://calentadoresalgen.com",
+        host: "calentadoresalgen.com",
+        open: "external"
+    });
+    gulp.watch('style.scss',['sass']);
+    gulp.watch("app/*.html").on('change', browserSync.reload);
+})
